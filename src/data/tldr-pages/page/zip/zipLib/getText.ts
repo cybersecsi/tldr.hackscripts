063505c8870d384a7bf87de2658ffe31c3a2f0b2
gx1 <18548727+giper45@users.noreply.github.com>
2024-07-31 18:30:25 +0200

    fix getText typescript error and package.json version from public github tldr-pages

--- src/data/tldr-pages/page/zip/zipLib/getText.ts
+++ src/data/tldr-pages/page/zip/zipLib/getText.ts
@@ -2,6 +2,13 @@ import type { Entry } from "@zip.js/zip.js";
 
 export async function getText(entry: Entry): Promise<string> {
   const { TextWriter } = await import("./zip-js-lib");
-  const text = await entry.getData(new TextWriter());
+  let text = '';
+  if (entry && typeof entry.getData === 'function') {
+    const text = await entry.getData(new TextWriter());
+  } 
+  else {
+    throw new Error('Text cannot be empty');
+
+  }
   return text;
 }
